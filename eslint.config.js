92ad78cc261fff5258d9b37bf9633053a30e4b81
Owen <chohj7636@gmail.com>
2024-12-12 11:55:33 +0900

    eslint rules 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -23,6 +23,10 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // 사용하지 않는 변수가 있을 때 발생하는 경고 비활성화
+      '@typescript-eslint/no-unused-vars': ['off'],
+      // 콘솔 사용 시 발생하는 경고 비활성화
+      'no-console': ['off'],
     },
   },
-)
+);
